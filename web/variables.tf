60476fd89790ce547f97478a9a7f39c67f0e2624
[Erik <eje1992@live.com>
2024-10-13 21:11:24 +0200

    source content web

--- web/variables.tf
+++ web/variables.tf
@@ -22,7 +22,7 @@ variable "sa_name" {
 variable "source_content" {
   description = "The content of the index.html file"
   type        = string
-  default     = "<h1>Made with Terraform - CI/CD</h1>"
+  default     = "<h1>This was made with Terraform - CI/CD</h1>"
 
 }
 
